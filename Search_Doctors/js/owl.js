1571ff23912b32fb19ced0b091b8316647b1cb8f
Arwa Mohamed <arwamohamed44.am@gmail.com>
2022-02-28 00:46:40 +0200

    add section appoitments to doctor's card

--- Search_Doctors/js/owl.js
+++ Search_Doctors/js/owl.js
@@ -11,14 +11,42 @@
                 items:1,
                 nav:true
             },
-            600:{
+            1315:{
                 items:2,
-                nav:false
+                nav:true
+            },
+            1500:{
+                items:3,
+                nav:true,
+                loop:true,
+                mergeFit:true
+            }
+        }
+    });
+
+
+
+    $('.owl-two').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        
+        responsiveClass:true,
+
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:true
             },
             1000:{
                 items:3,
                 nav:true,
-                loop:true
+                loop:true,
+                mergeFit:true
             }
         }
     });
