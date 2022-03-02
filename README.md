985f48c6fd32970b57037c07718b8d4d11bd7dc8
Abdelrhman-1998 <abdelrhman.ashraf.pro@gmail.com>
2022-03-02 11:59:16 +0200

    creating angular project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# A4Health
+# AngularProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'A4Health'`, () => {
+  it(`should have as title 'Angular_project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('A4Health');
+    expect(app.title).toEqual('Angular_project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('A4Health app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular_project app is running!');
   });
 });
