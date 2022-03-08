4e63e960a0b13d674819a5d76572524b497ed625
AhmedAyman16899 <36383074+AhmedAyman16899@users.noreply.github.com>
2022-03-08 09:48:20 +0200

    update doctor view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoctorAppiontmentComponent } from './Doctor/doctor-appiontment/doctor-appiontment.component';
+import { DoctorDashboardComponent } from './Doctor/doctor-dashboard/doctor-dashboard.component';
+import { DoctorEditProfileComponent } from './Doctor/doctor-edit-profile/doctor-edit-profile.component';
+import { DoctorFeedbackComponent } from './Doctor/doctor-feedback/doctor-feedback.component';
+import { DoctorProfileComponent } from './Doctor/doctor-profile/doctor-profile.component';
+import { DoctorReservationsComponent } from './Doctor/doctor-reservations/doctor-reservations.component';
 import { UserChangePasswordComponent } from './user-change-password/user-change-password.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { UserManageProfileComponent } from './user-manage-profile/user-manage-profile.component';
 import { UserReservationsComponent } from './user-reservations/user-reservations.component';
 
 const routes: Routes = [
-  {path:'reservations' , component:UserReservationsComponent},
-  {path:'manageprofile' , component:UserManageProfileComponent},
-  {path:'changepassword' , component:UserChangePasswordComponent}
+  {path:'userdashboard' , component:UserDashboardComponent , children:[
+    {path:'reservations' , component:UserReservationsComponent},
+    {path:'manageprofile' , component:UserManageProfileComponent},
+    {path:'changepassword' , component:UserChangePasswordComponent},
+  ]},
+  {path:'doctordashboard' , component:DoctorDashboardComponent , children:[
+    {path:'profile' , component:DoctorProfileComponent},
+    {path:'editprofile' , component:DoctorEditProfileComponent},
+    {path:'appointment' , component:DoctorAppiontmentComponent},
+    {path:'dreservations' , component:DoctorReservationsComponent},
+    {path:'feedback' , component:DoctorFeedbackComponent},
+  ]},
+ 
 ];
 
 @NgModule({
