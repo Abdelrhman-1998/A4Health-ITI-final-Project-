51488bfa2a0f53abf9b8a8cb186c47a54a1831ea
Arwa Mohamed <arwamohamed44.am@gmail.com>
2022-03-10 17:33:31 +0200

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddDoctorComponent } from './Component/Admin/add-doctor/add-doctor.component';
 import { AddSpecialtiesComponent } from './Component/Admin/add-specialties/add-specialties.component';
+import { AdminDashboardComponent } from './Component/Admin/admin-dashboard/admin-dashboard.component';
 import { AdminLoginComponent } from './Component/Admin/admin-login/admin-login.component';
 import { DoctorsComponent } from './Component/Admin/doctors/doctors.component';
 import { FeedbacksComponent } from './Component/Admin/feedbacks/feedbacks.component';
@@ -9,15 +10,19 @@ import { PatientComponent } from './Component/Admin/patient/patient.component';
 import { SpecialtiesComponent } from './Component/Admin/specialties/specialties.component';
 import { SearchResultsComponent } from './search_doctors/main/search_results/search-results/search-results.component';
 const routes: Routes = [
-  {path:'admin',component:AdminLoginComponent},
-  {path:'admin/addDoctor',component:AddDoctorComponent},
-  {path:'admin/doctor',component:DoctorsComponent},
-  {path:'admin/patient',component:PatientComponent},
-  {path:'admin/specialties',component:SpecialtiesComponent},
-  {path:'admin/addSpecialties',component:AddSpecialtiesComponent},
-  {path:'admin/specialty/edit/:id',component:AddSpecialtiesComponent} ,
-  {path:'admin/feadback',component:FeedbacksComponent},
+
+  {path:'admin/login',component:AdminLoginComponent},
+  {path:'admin',component:AdminDashboardComponent,children:[
+    {path:'addDoctor',component:AddDoctorComponent},
+    {path:'doctor',component:DoctorsComponent},
+    {path:'patient',component:PatientComponent},
+    {path:'specialties',component:SpecialtiesComponent},
+    {path:'addSpecialties',component:AddSpecialtiesComponent},
+    {path:'specialty/edit/:id',component:AddSpecialtiesComponent} ,
+    {path:'feadback',component:FeedbacksComponent},
  
+  ]},
+  
 ];
 
 @NgModule({
